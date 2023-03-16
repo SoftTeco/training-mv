83b9d198440ac7e71b5a412bb34f6f65baf7b1c8
isostheneia94 <maksim1005001337228322@gmail.com>
2023-03-16 15:44:37 +0300

    add all files from branch terraform from max_verbitskiy github

---
+++ client/hooks/useTitle.js
@@ -0,0 +1,36 @@
+import {useState} from "react";
+//import Config from "./config.json";
+//import env from "./.env";
+//import {port} from './vars.js'; 
+//import {} from './vars.js';
+
+export const useTitle = ()=>{
+    const [resp,setResp] = useState();
+    //const port = Config.PORT;
+    //const envPort = process.env.PORT;
+    //let url = `http://${domain}:${port}/graphql`;
+    let url = `http://wp-db-js-wordpress-service.k8s-${ENVIRONMENT}.svc.cluster.local:8000/graphql`;
+    //let url = `http://wordpress:80`;
+    //let url = `http://localhost:${envPort}/graphql`;
+    const getResp = async()=>{
+        const res= await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query: `
+            query MyQuery {
+              posts {
+                nodes {
+                  title
+                  uri
+                }
+              }
+            }
+            `
+            }),
+        })
+            .then(res => res.json())
+            .then(res => res.data);
+        setResp(res)
+    }
+    return {resp,getResp,status:!!resp}
+}
