c581e66df7978fbe92c87b58d8e57d699374e01f
isostheneia94 <maksim1005001337228322@gmail.com>
2023-03-28 12:59:56 +0300

    add script, for auto using act for dev and prod pipeline. Feature: every new namespace which will be created, has extended number in the end of name. Via script, created number the same for both namespaces: dev and prod. also you have to write value of variables connected to script.  - your token,  - version of js image for dev env,  - version of wordpress image for dev env,  - version of js image for prod env,  - version of wordpress image for prod env.

--- execute_dev_and_prod.sh
+++ execute_dev_and_prod.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 NUMBER=$(date +"%d%H%M")
 
 act --workflows .github/workflows/terraform/main_cd_dev.yml -s GITHUB_TOKEN=$1 --input js_image_dev=$2 --input wp_image_dev=$3 --input namespace_extended_number=$NUMBER
