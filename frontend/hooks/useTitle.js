5680f6c0f1ab261c3241f98ba4cba8999622c48a
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-04-18 16:51:19 +0300

    fix(FA-49): changes
    - changed ci pipeline for the presence of envs and builded arguments
    
    - changed Dockerfile: deleted arguments, and echo commands to .env file
    
    - changed url to backend on js hook file

--- frontend/hooks/useTitle.js
+++ frontend/hooks/useTitle.js
@@ -1,17 +1,8 @@
 import {useState} from "react";
-//import Config from "./config.json";
-//import env from "./.env";
-//import {port} from './vars.js'; 
-//import {} from './vars.js';
-//import {API_URL} from './.env';
+
 export const useTitle = ()=>{
     const [resp,setResp] = useState();
-    //const port = Config.PORT;
-    //const envPort = process.env.PORT;
-    //let url = `http://${domain}:${port}/graphql`;
     //let url = `http://wp-db-js-wordpress-service.k8s-${ENVIRONMENT}.svc.cluster.local:8000/graphql`;
-    //let url = `http://wordpress:80`;
-    //let url = `http://localhost:8003/graphql`;
     let url = `${process.env.NEXT_PUBLIC_API_URL}`;
     const getResp = async()=>{
         const res= await fetch(url, {
