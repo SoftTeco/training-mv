aed66324407dd26731892738c02283c17ab8b8a2
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-05-29 15:03:13 +0300

    fix(FA-58): pipeline changes
    - fixed working-directory and deleted env variables for creating aks

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -1,37 +1,29 @@
 resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+  prefix = "rg-wp-db-js-${var.environment}"
 }
-
-#data "azurerm_resource_group" "wp-db-js_rg" {
-#  name     = "NetworkWatcherRG"
-  #location = "westeurope"
-#}
-
-#data "azurerm_resources" "example" {
-#  resource_group_name = azurerm_resouce_group.wp-db-js_rg.name
-#}
-
-#resource "azurerm_resource_group" "wp-db-js_rg" {
-#  name     = random_pet.rg_name.prefix
-#  location = var.resource_group_location
-#}
-
 
 resource "random_id" "log_analytics_workspace_name_suffix" {
   byte_length = 8
 }
 
-resource "azurerm_kubernetes_cluster" "wp-db-js_k8s" {
-  name                = var.aks_service_cluster_name
-  location            = var.aks_service_resource_group_location
+resource "azurerm_log_analytics_workspace" "workspace-wp-db-js" {
+  name                = "wp-db-js-${var.environment}"
+  location            = "centralindia"
+  resource_group_name = "WPDBJS-RG"
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_kubernetes_cluster" "aks-wp-db-js" {
+  name                = "aks-wp-db-js-${var.environment}"
+  location            = var.rg_location
   resource_group_name = "NetworkWatcherRG"
-  dns_prefix          = var.aks_service_dns_prefix
-  #address_space       = ["10.0.0.0/16"]
+  dns_prefix          = "wp-db-js-${var.environment}"
 
   default_node_pool {
-    name       = var.aks_service_node_name
-    node_count = var.aks_service_agent_count
-    vm_size    = var.aks_service_node_vm_size
+    name       = "wpdbjs${var.environment}"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
   }
 
   identity {
@@ -39,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "wp-db-js_k8s" {
   }
 
   tags = {
-    Environment = "Development"
+    Environment = var.environment
   }
 
   network_profile {
@@ -47,3 +39,4 @@ resource "azurerm_kubernetes_cluster" "wp-db-js_k8s" {
     load_balancer_sku = "standard"
   }
 }
+
--- terraform/aks/terraform.tfvars
+++ terraform/aks/terraform.tfvars
@@ -1,4 +1,3 @@
 rg_location = "centralindia"
 node_vm_size = "Standard_B2ms"
 node_count = 2
-#environment = "dev"
