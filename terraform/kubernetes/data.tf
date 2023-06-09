f10de127597e9738aba54300c2730bef1eac29bd
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-06-09 17:24:10 +0300

    fix(FA-58): terraform
    - added tf data for saving existing resources before executed

---
+++ terraform/kubernetes/data.tf
@@ -0,0 +1,27 @@
+locals {
+  name = "${terraform.workspace}"
+}
+
+data "azurerm_resource_group" "rg-wpdbjs" {
+  name     = "RG-WPDBJS-${local.name}"
+  location = var.rg_location
+}
+
+data "azurerm_kubernetes_cluster" "aks-wpdbjs" {
+  name                = "aks-WPDBJS-${local.name}"
+  location            = var.rg_location
+  resource_group_name = azurerm_resource_group.rg-wpdbjs.name
+  dns_prefix          = "pref-wpdbjs"
+
+
+  default_node_pool {
+    name       = "wpdbjs${local.name}"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
