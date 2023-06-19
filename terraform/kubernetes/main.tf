fa85d9643edeb032b1ad9bd2ead06d7b6fc9c2ec
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-06-19 16:35:36 +0300

    fix(FA-58): terraform
    - deleted 'behavior' in kuberenetes_hpa

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -260,37 +260,37 @@ resource "kubernetes_horizontal_pod_autoscaler_v1" "ascale-wpdbjs-frontend" {
       api_version = "apps/v1"
     }
 
-    behavior {
-      scale_down {
-        stabilization_window_seconds = 300
-        select_policy                = "Min"
-        policy {
-          period_seconds = 120
-          type           = "Pods"
-          value          = 1
-        }
+    #behavior {
+      #scale_down {
+        #stabilization_window_seconds = 300
+        #select_policy                = "Min"
+        #policy {
+      #     period_seconds = 120
+      #     type           = "Pods"
+      #     value          = 1
+      #   }
 
-        policy {
-          period_seconds = 310
-          type           = "Percent"
-          value          = 100
-        }
-      }
-      scale_up {
-        stabilization_window_seconds = 600
-        select_policy                = "Max"
-        policy {
-          period_seconds = 180
-          type           = "Percent"
-          value          = 100
-        }
-        policy {
-          period_seconds = 600
-          type           = "Pods"
-          value          = 3
-        }
-      }
-    }
+      #   policy {
+      #     period_seconds = 310
+      #     type           = "Percent"
+      #     value          = 100
+      #   }
+      # }
+      # scale_up {
+      #   stabilization_window_seconds = 600
+      #   select_policy                = "Max"
+      #   policy {
+      #     period_seconds = 180
+      #     type           = "Percent"
+      #     value          = 100
+      #   }
+      #   policy {
+      #     period_seconds = 600
+      #     type           = "Pods"
+      #     value          = 3
+      #   }
+      # }
+    #}
   }
 }
 
@@ -309,37 +309,37 @@ resource "kubernetes_horizontal_pod_autoscaler_v1" "ascale-wpdbjs-wordpress" {
       api_version = "apps/v1"
     }
 
-    behavior {
-      scale_down {
-        stabilization_window_seconds = 300
-        select_policy                = "Min"
-        policy {
-          period_seconds = 120
-          type           = "Pods"
-          value          = 1
-        }
+    # behavior {
+    #   scale_down {
+    #     stabilization_window_seconds = 300
+    #     select_policy                = "Min"
+    #     policy {
+    #       period_seconds = 120
+    #       type           = "Pods"
+    #       value          = 1
+    #     }
 
-        policy {
-          period_seconds = 310
-          type           = "Percent"
-          value          = 100
-        }
-      }
-      scale_up {
-        stabilization_window_seconds = 600
-        select_policy                = "Max"
-        policy {
-          period_seconds = 180
-          type           = "Percent"
-          value          = 100
-        }
-        policy {
-          period_seconds = 600
-          type           = "Pods"
-          value          = 3
-        }
-      }
-    }
+    #     policy {
+    #       period_seconds = 310
+    #       type           = "Percent"
+    #       value          = 100
+    #     }
+    #   }
+    #   scale_up {
+    #     stabilization_window_seconds = 600
+    #     select_policy                = "Max"
+    #     policy {
+    #       period_seconds = 180
+    #       type           = "Percent"
+    #       value          = 100
+    #     }
+    #     policy {
+    #       period_seconds = 600
+    #       type           = "Pods"
+    #       value          = 3
+    #     }
+    #   }
+    # }
   }
 }
 
@@ -358,37 +358,37 @@ resource "kubernetes_horizontal_pod_autoscaler_v1" "ascale-wpdbjs-mysql" {
       api_version = "apps/v1"
     }
 
-    behavior {
-      scale_down {
-        stabilization_window_seconds = 300
-        select_policy                = "Min"
-        policy {
-          period_seconds = 120
-          type           = "Pods"
-          value          = 1
-        }
+    # behavior {
+    #   scale_down {
+    #     stabilization_window_seconds = 300
+    #     select_policy                = "Min"
+    #     policy {
+    #       period_seconds = 120
+    #       type           = "Pods"
+    #       value          = 1
+    #     }
 
-        policy {
-          period_seconds = 310
-          type           = "Percent"
-          value          = 100
-        }
-      }
-      scale_up {
-        stabilization_window_seconds = 600
-        select_policy                = "Max"
-        policy {
-          period_seconds = 180
-          type           = "Percent"
-          value          = 100
-        }
-        policy {
-          period_seconds = 600
-          type           = "Pods"
-          value          = 3
-        }
-      }
-    }
+    #     policy {
+    #       period_seconds = 310
+    #       type           = "Percent"
+    #       value          = 100
+    #     }
+    #   }
+    #   scale_up {
+    #     stabilization_window_seconds = 600
+    #     select_policy                = "Max"
+    #     policy {
+    #       period_seconds = 180
+    #       type           = "Percent"
+    #       value          = 100
+    #     }
+    #     policy {
+    #       period_seconds = 600
+    #       type           = "Pods"
+    #       value          = 3
+    #     }
+    #   }
+    # }
   }
 }
 
