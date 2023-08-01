6a6e72d6afc9fef83486db60305d3641bca316bc
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-08-01 12:21:35 +0300

    fix(FA-58): terraform
    -  changed wordpress image to 1.28.0 version (new, with entryp commands)

--- wordpress/entrypoint-script.sh
+++ wordpress/entrypoint-script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
-service apache2 reload
+apt install sudo
+sudo su
+sudo chmod 666 /etc/apache2/apache2.conf
+sudo echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
+sudo service apache2 reload
