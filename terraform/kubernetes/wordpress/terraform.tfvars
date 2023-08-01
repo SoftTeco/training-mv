1aa28e2a4c51727722d0d86336760152ad6a3541
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-08-01 16:21:40 +0300

    fix(FA-58): terraform
    -  changed wordpress image to 1.28.5 version (new, with entryp commands)

--- terraform/kubernetes/wordpress/terraform.tfvars
+++ terraform/kubernetes/wordpress/terraform.tfvars
@@ -1,4 +1,4 @@
-wordpress-image         = "1.28.4"
+wordpress-image         = "1.28.5"
 registry                = "ghcr.io"
 wordpress-target-port   = 8000
 wordpress-deploy-port   = 8000
--- wordpress/entrypoint-script.sh
+++ wordpress/entrypoint-script.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get install sudo
-sudo su
+#sudo su
 sudo chmod 666 /etc/apache2/apache2.conf
 sudo echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
 sudo service apache2 reload
