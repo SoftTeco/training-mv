93cf822406d5372afefe73a408d1b8f9fc534776
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-08-03 16:48:12 +0300

    ci(FA-58): wordpress
    -  fix apache2 conf

--- wordpress/entrypoint-script.sh
+++ wordpress/entrypoint-script.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-apt-get update
-apt-get install sudo
+#hostname=`hostname -I | awk '{print $1}'`
+/etc/init.d/apache2 start
+#sudo service apache2 restart
 #sudo su
 sudo chmod 666 /etc/apache2/apache2.conf
 sudo echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
-sudo service apache2 reload
+#sudo echo "ServerName $hostname" >> /etc/apache2/apache2.conf
+#sudo service apache2 restart
+sudo /etc/init.d/apache2 reload
