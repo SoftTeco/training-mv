71d9ad0d2b98c4ac99360b8ba8c5be339f17afa9
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-08-09 13:07:52 +0300

    ci(FA-58): wordpress
    -  added 81 port and changed all configs

--- wordpress/entrypoint-script.sh
+++ wordpress/entrypoint-script.sh
@@ -7,6 +7,7 @@
 sudo chmod 666 /etc/apache2/apache2.conf
 sudo echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
 sudo sed -i -e '6 s/^/Listen 8000\n/;' /etc/apache2/ports.conf
+sudo sed -i -e '6 s/^/Listen 81\n/;' /etc/apache2/ports.conf
 sudo ln -s /etc/apache2/sites-available/wordpress.conf /etc/apache2/sites-enabled/wordpress.conf
 #sudo echo "ServerName $hostname" >> /etc/apache2/apache2.conf
 #sudo service apache2 restart
