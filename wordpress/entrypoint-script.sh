d7d2e67950d25954103ee3af970b3c9b8574ba2b
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-08-09 14:23:31 +0300

    fix(FA-58): wordpress image new step
    -  added step 'test echo release version'

--- wordpress/entrypoint-script.sh
+++ wordpress/entrypoint-script.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-#hostname=`hostname -I | awk '{print $1}'`
 /etc/init.d/apache2 start
 sudo chmod 666 /etc/apache2/apache2.conf
 sudo echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
 sudo sed -i -e '6 s/^/Listen 8000\n/;' /etc/apache2/ports.conf
 sudo sed -i -e '6 s/^/Listen 81\n/;' /etc/apache2/ports.conf
 sudo ln -s /etc/apache2/sites-available/wordpress.conf /etc/apache2/sites-enabled/wordpress.conf
-#sudo echo "ServerName $hostname" >> /etc/apache2/apache2.conf
+
 sudo /etc/init.d/apache2 reload
 sudo /etc/init.d/apache2 restart
