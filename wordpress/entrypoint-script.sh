5c485035015673339143c6676b49323c752eb8b1
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-08-09 13:54:03 +0300

    ci(FA-58): wordpress
    -  triying to build image

--- wordpress/entrypoint-script.sh
+++ wordpress/entrypoint-script.sh
@@ -2,13 +2,11 @@
 
 #hostname=`hostname -I | awk '{print $1}'`
 /etc/init.d/apache2 start
-#sudo service apache2 restart
-#sudo su
 sudo chmod 666 /etc/apache2/apache2.conf
 sudo echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
 sudo sed -i -e '6 s/^/Listen 8000\n/;' /etc/apache2/ports.conf
 sudo sed -i -e '6 s/^/Listen 81\n/;' /etc/apache2/ports.conf
 sudo ln -s /etc/apache2/sites-available/wordpress.conf /etc/apache2/sites-enabled/wordpress.conf
 #sudo echo "ServerName $hostname" >> /etc/apache2/apache2.conf
-#sudo service apache2 restart
 sudo /etc/init.d/apache2 reload
+sudo /etc/init.d/apache2 restart
