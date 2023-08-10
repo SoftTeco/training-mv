0303e7fafbe0a356b9de1dec122b24ae4f5e173f
Max Verbitskiy <m.verbitskiyy@softteco.com>
2023-08-10 15:01:15 +0300

    fix(FA-58): wordpress image
    -  added instructions about WP_HOME to wp-config.php

--- wordpress/entrypoint-script.sh
+++ wordpress/entrypoint-script.sh
@@ -4,11 +4,14 @@
 sudo chmod 666 /etc/apache2/apache2.conf
 sudo echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
 sudo sed -i -e '6 s/^/Listen 8000\n/;' /etc/apache2/ports.conf
-sudo sed -i -e '6 s/^/Listen 81\n/;' /etc/apache2/ports.conf
+#sudo sed -i -e '6 s/^/Listen 81\n/;' /etc/apache2/ports.conf
 sudo ln -s /etc/apache2/sites-available/wordpress.conf /etc/apache2/sites-enabled/wordpress.conf
 sudo a2ensite wordpress
 sudo a2enmod rewrite
 sudo a2dissite 000-default
+
+sudo echo "define( 'WP_HOME', 'http://localhost:8000' );" >> /var/www/html/wp-config.php
+sudo echo "define( 'WP_SITEURL', 'http://localhost:8000' );" >> /var/www/html/wp-config.php
 
 sudo /etc/init.d/apache2 reload
 sudo /etc/init.d/apache2 restart
